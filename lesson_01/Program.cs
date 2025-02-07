89f70ef8fb6866f5b9787afa1cc0d859a8c59424
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-07 18:53:51 +0300

    Добавьте файлы проекта.

---
+++ lesson_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше Имя: ");
+            string name;
+            name = Console.ReadLine();
+            if (name == "")
+                Console.WriteLine("Привет, Мир!!!");
+            else
+                Console.WriteLine("Привет, " + name + "!");
+
+        }
+    }
+}
