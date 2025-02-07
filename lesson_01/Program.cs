c489083e40d0635d48b8388a9c3b01e966cb9595
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-07 20:58:48 +0300

    Задача 2 Пользователь вводит с клавиатуры четыре цифры. Необходимо создать число, содержащее эти цифры. Например, если с клавиатуры введено 1, 5, 7, 8 тогда нужно сформировать число 1578. Числа вводим по одному после каждого нажимая Enter. После ввода 4 цифр формируем их значение типа int и выводим в консоль

--- lesson_01/Program.cs
+++ lesson_01/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,30 @@ namespace lesson_01
     {
         static void Main(string[] args)
         {
+            String num = "";
+            String mas = "";
+            while(true)
+            {
+                Console.WriteLine("Введите 4 цифры: ");
+                num = Console.ReadLine();
+                if (num == "end") break;
+                mas += num;
+            }
+            int a = Convert.ToInt32(mas) + 1;
+            Console.WriteLine("Число num + 1" + a);
+        }
+
+        void fizz_puzz()
+        {
             Console.WriteLine("Введите число: ");
 
             int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Остаток от деления на 3: {num%3}");
+            Console.WriteLine($"Остаток от деления на 3: {num % 3}");
 
             if (num <= 0 || num >= 100) Console.WriteLine("Error");
             Console.WriteLine(num % 3 == 0 ? "Fizz" : null);
             Console.WriteLine(num % 5 == 0 ? "Buzz" : null);
-            Console.WriteLine(num % 3 != 0 && num % 5 != 0 ? Convert.ToString(num): null);
-
-
+            Console.WriteLine(num % 3 != 0 && num % 5 != 0 ? Convert.ToString(num) : null);
         }
     }
 }
