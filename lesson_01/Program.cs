c15d012948eb8905d887c59777c9d3d854ca371d
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-07 19:34:19 +0300

    По умолчанию платформа .NET предоставляет ряд методов, которые позволяют преобразовать различные значения к типам int, double и т.д. Некоторые из этих методов: -Convert.ToInt32() (преобразует к типу int) -Convert.ToDouble() (преобразует к типу double) -Convert.ToDecimal() (преобразует к типу decimal)

--- lesson_01/Program.cs
+++ lesson_01/Program.cs
@@ -10,10 +10,19 @@ namespace lesson_01
     {
         static void Main(string[] args)
         {
-            string name = "Tom";
-            int age = 34;
-            double height = 1.7;
-            Console.Write($"Имя: {name}  Возраст: {age}  Рост: {height}м");
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите рост: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите размер зарплаты: ");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {height}м  Зарплата: {salary}$");
         }
     }
 }
