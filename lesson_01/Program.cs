4930913ec66c2300644fd79201e44116c61d1ba9
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-07 19:23:51 +0300

    Нередко возникает необходимость вывести на консоль в одной строке значения сразу нескольких переменных. В этом случае мы можем использовать прием, который называется интерполяцией:

--- lesson_01/Program.cs
+++ lesson_01/Program.cs
@@ -10,11 +10,10 @@ namespace lesson_01
     {
         static void Main(string[] args)
         {
-            string hello = "Привет мир";
-            Console.WriteLine(hello);
-            Console.WriteLine("Добро пожаловать в C#!");
-            Console.WriteLine("Пока мир...");
-            Console.WriteLine(24.5);
+            string name = "Tom";
+            int age = 34;
+            double height = 1.7;
+            Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {height}м");
         }
     }
 }
