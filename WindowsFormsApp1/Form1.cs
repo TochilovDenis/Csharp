afb6defa343fa85548d914993505a9a48666d72b
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-12 18:29:32 +0300

    Создаем калькулятора

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -13,23 +13,59 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         public int sum = 0;
-        public String fnc = "+"; 
+        public int var = 0;
+        public String fnc = "+";
 
         public Form1()
         {
             InitializeComponent();
+            textBox1.Text = sum.ToString();
+            textBox2.Text = fnc;
+            textBox3.Text = var.ToString(); 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            calc(1);
+            var = 1;
+            textBox3.Text = "1";
+
         }
 
-        public void calc(int n) {
-            if (fnc == "+")
-                sum = sum + n;
-               
-            textBox1.Text = "sum = " + sum;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var = 2;
+            textBox3.Text = "2";
+
         }
-    }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            calc();
+        }
+
+
+        public void calc()
+        {
+            if (fnc == "+")
+                sum = sum + var;
+
+            if (fnc == "-")
+                sum = sum - var;
+
+            textBox1.Text = sum.ToString();
+        }
+
+        private void button_sum_Click(object sender, EventArgs e)
+        {
+            fnc = "+";  
+            textBox2.Text = "+";
+        }
+
+        private void button_minus_Click(object sender, EventArgs e)
+        {
+            fnc = "-";
+            textBox2.Text = "-";
+        }
+
+    }   
 }
