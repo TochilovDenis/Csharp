fe28450b8ef610ea1e612c0ff832b7361b92f855
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-24 19:00:41 +0300

    Добавлен генератор случайных чисел

--- number_generator/Program.cs
+++ number_generator/Program.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace number_generator
 {
     class Program
     {
+        const int SIZE = 1000;
+        static int[] numbers = new int[SIZE];
         static void Main(string[] args)
         {
+            var random = new Random();
+            for (int i = 0; i < SIZE; i++)
+            {
+                numbers[i] = random.Next(1, 1000);
+                Console.WriteLine(numbers);
+            }
         }
     }
 }
+
+/* Консольное приложение генерирует набор чисел, состоящий из 1000 элементов.
+С помощью механизма потоков нужно найти максимум, минимум, среднее в этом наборе.
+для каждой из задач выделите поток. */
