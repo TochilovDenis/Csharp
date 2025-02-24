6f1b1f4a33d408f60581ea59b453d1b8bdb969c9
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-24 19:16:07 +0300

    Добавлен многопоточный алгоритм поиска максимального значения в массиве:
    - Реализована параллельная обработка массива размером 1000 элементов
    - Использована блокировка (lock) для потокобезопасного обновления результата
    - Добавлена обработка исключений для надежной работы
    - Оптимизирована производительность через локальные переменные
    - Обеспечена корректная синхронизация между потоками

--- number_generator/Program.cs
+++ number_generator/Program.cs
@@ -12,13 +12,39 @@ namespace number_generator
     {
         const int SIZE = 1000;
         static int[] numbers = new int[SIZE];
+        static int maxValue = int.MinValue;
+
+        static readonly object locker = new object();
+
         static void Main(string[] args)
         {
+            
             var random = new Random();
             for (int i = 0; i < SIZE; i++)
             {
-                numbers[i] = random.Next(1, 1000);
-                Console.WriteLine(numbers);
+                numbers[i] = random.Next(1, 10000);
+            }
+
+            Thread maxThread = new Thread(FindMaximum);
+
+            maxThread.Start();
+            maxThread.Join();
+            Console.WriteLine($"Максимальное значение: {maxValue}");
+        }
+
+
+        // Поиск максимальное число
+        static void FindMaximum()
+        {
+            int localMax = int.MinValue;
+            for (int i = 0; i < SIZE; i++)
+            {
+                if (numbers[i] > localMax) localMax = numbers[i];
+            }
+
+            lock (locker)
+            {
+                if (localMax > maxValue) maxValue = localMax;
             }
         }
     }
