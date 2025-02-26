be20768518377026190e8c856aa75bf6662157eb
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-26 18:39:11 +0300

    ...

---
+++ Parallel_programming_and_the_TPL_library/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_programming_and_the_TPL_library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -19,8 +19,8 @@ namespace WindowsFormsApp3
 
             //openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
             //saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
-        }
 
+        }
         private void button_open_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
