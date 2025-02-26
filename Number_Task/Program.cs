fea4c3ca5af30f22bc99ce0c85b7101563e825ca
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-26 20:17:25 +0300

    ...

--- Number_Task/Program.cs
+++ Number_Task/Program.cs
@@ -10,6 +10,26 @@ namespace Number_Task
     {
         static void Main(string[] args)
         {
+            Task number = Task.Run(() =>
+            {
+                for (int i = 2; i < 1001; i++)
+                {
+                    int res = 0;
+                    for (int j = 2; j * j <= i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            res += 1;
+                        }
+                    }
+                    if (res == 2)
+                    {
+                        Console.WriteLine($"Number : {i}");
+                    }
+                }
+            });
+            number.Wait();
+            Console.WriteLine("End main");
         }
     }
 }
