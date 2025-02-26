dfd56a4073889b58e67368dbc99c24e3297d402d
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-26 20:27:27 +0300

    Один из возможных консольных выводов программы:
    Завершение метода Main

--- Working_with_Task_class/Program.cs
+++ Working_with_Task_class/Program.cs
@@ -11,20 +11,32 @@ namespace Working_with_Task_class
     {
         static void Main(string[] args)
         {
-            Task[] tasks1 = new Task[3]
-            {
-                new Task(() => Console.WriteLine("First Task")),
-                new Task(() => Console.WriteLine("Second Task")),
-                new Task(() => Console.WriteLine("Third Task"))
-             };
-            // запуск задач в массиве
-            foreach (var t in tasks1)
-                t.Start();
+            //Task[] tasks1 = new Task[3]
+            //{
+            //    new Task(() => Console.WriteLine("First Task")),
+            //    new Task(() => Console.WriteLine("Second Task")),
+            //    new Task(() => Console.WriteLine("Third Task"))
+            // };
+            //// запуск задач в массиве
+            //foreach (var t in tasks1)
+            //    t.Start();
 
-            Task[] tasks2 = new Task[3];
-            int j = 1;
-            for (int i = 0; i < tasks2.Length; i++)
-                tasks2[i] = Task.Factory.StartNew(() => Console.WriteLine($"Task {j++}"));
+            //Task[] tasks2 = new Task[3];
+            //int j = 1;
+            //for (int i = 0; i < tasks2.Length; i++)
+            //    tasks2[i] = Task.Factory.StartNew(() => Console.WriteLine($"Task {j++}"));
+
+            Task[] tasks = new Task[3];
+            for (var i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = new Task(() =>
+                {
+                    Thread.Sleep(1000); // эмуляция долгой работы
+                    Console.WriteLine($"Task{i} finished");
+                });
+                tasks[i].Start();   // запускаем задачу
+            }
+            Console.WriteLine("Завершение метода Main");
         }
     }
 }
