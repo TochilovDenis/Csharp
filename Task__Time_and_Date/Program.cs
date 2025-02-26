aaa1f92a1b8e6485f22e4d632df52e5622727b5a
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-26 20:03:37 +0300

    Run

--- Task__Time_and_Date/Program.cs
+++ Task__Time_and_Date/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task__Time_and_Date
@@ -10,6 +11,25 @@ namespace Task__Time_and_Date
     {
         static void Main(string[] args)
         {
+            Task current_time_1 = new Task(() =>
+            {
+                Console.WriteLine($"Time new Task: {DateTime.Now}");
+            });
+            current_time_1.Start();
+
+            Task current_time_2 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine($"Time Task.Factory.StartNew: {DateTime.Now}");
+            });
+
+            Task current_time_3 = Task.Run(() =>
+            {
+                Console.WriteLine($"Time Task.Run: {DateTime.Now}");
+            });
+
+            current_time_1.Wait();
+            current_time_2.Wait();
+            current_time_3.Wait();
         }
     }
 }
