d3184a0e52e85cce399c2eb7b0e1a7a8257d05fb
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-26 19:32:37 +0300

    Создать приложение, использующее класс Task. Приложение должно отображать текущее время и дату. Запустить задачу три способами:  * Через метод Start класса Task;  * Через метод Task.Factory.StartNew;  * Через метод Task.Run;

---
+++ Task__Time_and_Date/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task__Time_and_Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
