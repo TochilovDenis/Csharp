dae10dfeeea676f146d95f9bb19e752c89f3a233
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-26 20:20:14 +0300

    Одна задача может запускать другую - вложенную задачу. При этом эти задачи выполняются независимо друг от друга. Несмотря на то, что здесь мы ожидаем выполнения внешней задачи, но вложенная задача может завершить выполнение даже после завершения метода Main: Outer task starting... End of Main

--- Working_with_Task_class/Program.cs
+++ Working_with_Task_class/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Working_with_Task_class
@@ -10,6 +11,18 @@ namespace Working_with_Task_class
     {
         static void Main(string[] args)
         {
+            var outer = Task.Factory.StartNew(() =>      // внешняя задача
+            {
+                Console.WriteLine("Outer task starting...");
+                var inner = Task.Factory.StartNew(() =>  // вложенная задача
+                {
+                    Console.WriteLine("Inner task starting...");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Inner task finished.");
+                });
+            });
+            outer.Wait(); // ожидаем выполнения внешней задачи
+            Console.WriteLine("End of Main");
         }
     }
 }
