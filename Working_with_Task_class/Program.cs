c788f43e6b35f6c9c3b87a53373f4479e63e9e97
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-26 20:33:00 +0300

    Задачи могут не только выполняться как процедуры, но и возвращать определенные результаты. Во-первых, чтобы получать из задачи не который результат, необходимо типизировать объект Task тем типом, объект которого мы хотим получить из задачи. Например, в примере выше мы ожидаем из задачи sumTask получить число типа int, соответственно типизируем объект Task данным типом - Task<int>.
    И, во-вторых, в качестве задачи должен выполняться метод, который возвращает данный тип объекта. Так, в данном случае у нас в качестве задачи выполняется метод Sum, которая принимаетдва числа и на выходе возвращает их сумму - значение типа int.
    
    Возвращаемое число будет храниться в свойстве Result: sumTask.Result. Нам не надо его приводить к типу int, оно уже само по себе будет представлять число.
    
    int result = sumTask.Result;
    
    При этом при обращении к свойству Result текущий поток останавливает выполнение и ждет, когда будет получен результат из выполняемой задачи.

--- Working_with_Task_class/Program.cs
+++ Working_with_Task_class/Program.cs
@@ -26,19 +26,28 @@ namespace Working_with_Task_class
             //for (int i = 0; i < tasks2.Length; i++)
             //    tasks2[i] = Task.Factory.StartNew(() => Console.WriteLine($"Task {j++}"));
 
-            Task[] tasks = new Task[3];
-            for (var i = 0; i < tasks.Length; i++)
-            {
-                tasks[i] = new Task(() =>
-                {
-                    Thread.Sleep(1000); // эмуляция долгой работы
-                    Console.WriteLine($"Task{i} finished");
-                });
-                tasks[i].Start();   // запускаем задачу
-            }
-            Console.WriteLine("Завершение метода Main");
+            //Task[] tasks = new Task[3];
+            //for (var i = 0; i < tasks.Length; i++)
+            //{
+            //    tasks[i] = new Task(() =>
+            //    {
+            //        Thread.Sleep(1000); // эмуляция долгой работы
+            //        Console.WriteLine($"Task{i} finished");
+            //    });
+            //    tasks[i].Start();   // запускаем задачу
+            //}
+            //Console.WriteLine("Завершение метода Main");
 
-            Task.WaitAll(tasks); // ожидаем завершения всех задач
+            //Task.WaitAll(tasks); // ожидаем завершения всех задач
+
+            int n1 = 4, n2 = 5;
+            Task<int> sumTask = new Task<int>(() => Sum(n1, n2));
+            sumTask.Start();
+
+            int result = sumTask.Result;
+            Console.WriteLine($"{n1} + {n2} = {result}"); // 4 + 5 = 9
+
+            int Sum(int a, int b) => a + b;
 
         }
     }
