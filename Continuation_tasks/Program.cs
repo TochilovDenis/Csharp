dc3d9a9e2cddeefe429ded8e81d34fe0f47f24bb
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-28 18:46:51 +0300

    Задачи продолжения похожи на методы обратного вызова, но фактически являются обычными задачами Task. Первая задача задается с помощью лямбда-выражения, которое просто выводит id этой задачи. Вторая задача - задача продолжения задается с помощью метода ContinueWith, который в качестве параметра принимает делегат Action<Task>. То есть метод PrintTask, который передается в вызов ContinueWith, должен принимать параметр типа Task.
    Благодаря передачи в метод параметра Task, мы можем получить различные свойства предыдущей задачи, как например, в данном случае получает ее Id.
    
    И после завершения задачи task1 сразу будет вызываться задача task2. Консольный вывод программы:
    Id задачи: 2
    Id задачи: 1
    Id предыдущей задачи: 2
    Конец метода Main

--- Continuation_tasks/Program.cs
+++ Continuation_tasks/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Continuation_tasks
@@ -10,6 +11,27 @@ namespace Continuation_tasks
     {
         static void Main(string[] args)
         {
+            Task task1 = new Task(() =>
+            {
+                Console.WriteLine($"Id задачи: {Task.CurrentId}");
+            });
+
+            // задача продолжения - task2 выполняется после task1
+            Task task2 = task1.ContinueWith(PrintTask);
+
+            task1.Start();
+
+            // ждем окончания второй задачи
+            task2.Wait();
+            Console.WriteLine("Конец метода Main");
+
+
+            void PrintTask(Task t)
+            {
+                Console.WriteLine($"Id задачи: {Task.CurrentId}");
+                Console.WriteLine($"Id предыдущей задачи: {t.Id}");
+                Thread.Sleep(3000);
+            }
         }
     }
 }
