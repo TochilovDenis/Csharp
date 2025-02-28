0ee0cf89357c8a543441c7fea6f2de86c5ecfba0
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-28 21:03:36 +0300

    Добавить отмену выполнения задач при нахождении простого числа

--- Checking_number/Program.cs
+++ Checking_number/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Checking_number
@@ -13,26 +14,42 @@ namespace Checking_number
             /*  1 Создаем функцию для проверки является ли число простым
                 2 используем Parallel.For(int a, int b, Action<int>) для нахождения простых чисел от a до b*/
 
-            ParallelLoopResult result = Parallel.For(1, 1000, prime_number);
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancelTokenSource.Token;
+
+            ParallelLoopResult result = Parallel.For(1, 10000, prime_number);
 
             if (!result.IsCompleted)
                 Console.WriteLine($"Выполнение цикла завершено на итерации {result.LowestBreakIteration}");
 
+            // вычисляем квадрат числа
+
+
             void prime_number(int n)
             {
+
                 int res = 0;
-                Console.WriteLine($"Проверяем {n}");
-                for (int i = 1; i < n; i++)
+
+                for (int i = 1; i < n + 1; i++)
                 {
+                    if (token.IsCancellationRequested)  // проверяем наличие сигнала отмены задачи
+                    {
+                        return;     //  выходим из метода и тем самым завершаем задачу
+                    }
+
                     if (n % i == 0)
                     {
                         res += 1;
                     }
-                    if (res == 2)
-                    {
-                        Console.WriteLine($"Простое число : {n}");
-                    }
                 }
+
+                if (res == 2)
+                {
+                    Console.WriteLine($"Простое число : {n}");
+
+                    cancelTokenSource.Cancel();
+                }
+
             }
         }
     }
