b927187239f6900b9a1fe1555894c512d749c654
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-28 20:41:21 +0300

    Отмена задачи с помощью генерации исключения Второй способ завершения задачи представляет генерация исключения OperationCanceledException. Для этого применяется метод ThrowIfCancellationRequested() объекта CancellationToken.
    Здесь опять же проверяем значение свойства IsCancellationRequested, и если оно равно true, генерируем исключение:
      if (token.IsCancellationRequested)
          token.ThrowIfCancellationRequested(); // генерируем исключение
    
    Чтобы обработать исключение, помещаем весь код работы с задачей в конструкцию try..catch и также с помощью вызова cancelTokenSource.Cancel() посылаем сообщение об отмене задачи.
    
    Стоит отметить, что генерируемое исключение будет спрятано в объекте AggregateException, который по сути представляет набор исключений. Если причина исключения состояла в отмене задачи, то мы можем найти в этом наборе исключений исключение типа TaskCanceledException.
    
      catch (AggregateException ae)
      {
          foreach (Exception e in ae.InnerExceptions)
          {
              if (e is TaskCanceledException)
                  Console.WriteLine("Операция прервана");
              else
                  Console.WriteLine(e.Message);
          }
      }
    
    Класс TaskCanceledException является производным от OperationCanceledException. Исключение типа TaskCanceledException возникает, если для задачи устанавливается статус Canceled.
    
    Консольный вывод программы:
    
    Квадрат числа 1 равен 1
    Квадрат числа 2 равен 4
    Квадрат числа 3 равен 9
    Квадрат числа 4 равен 16
    Квадрат числа 5 равен 25
    Операция прервана
    Task Status: Canceled

--- Cancellation_Token/Program.cs
+++ Cancellation_Token/Program.cs
@@ -14,30 +14,43 @@ namespace Cancellation_Token
             CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
             CancellationToken token = cancelTokenSource.Token;
 
-            // задача вычисляет квадраты чисел
             Task task = new Task(() =>
             {
                 for (int i = 1; i < 10; i++)
                 {
-                    if (token.IsCancellationRequested)  // проверяем наличие сигнала отмены задачи
-                    {
-                        Console.WriteLine("Операция прервана");
-                        return;     //  выходим из метода и тем самым завершаем задачу
-                    }
+                    if (token.IsCancellationRequested)
+                        token.ThrowIfCancellationRequested(); // генерируем исключение
+
                     Console.WriteLine($"Квадрат числа {i} равен {i * i}");
                     Thread.Sleep(200);
                 }
             }, token);
-            task.Start();
+            try
+            {
+                task.Start();
+                Thread.Sleep(1000);
+                // после задержки по времени отменяем выполнение задачи
+                cancelTokenSource.Cancel();
 
-            Thread.Sleep(1000);
-            // после задержки по времени отменяем выполнение задачи
-            cancelTokenSource.Cancel();
-            // ожидаем завершения задачи
-            Thread.Sleep(1000);
+                task.Wait(); // ожидаем завершения задачи
+            }
+            catch (AggregateException ae)
+            {
+                foreach (Exception e in ae.InnerExceptions)
+                {
+                    if (e is TaskCanceledException)
+                        Console.WriteLine("Операция прервана");
+                    else
+                        Console.WriteLine(e.Message);
+                }
+            }
+            finally
+            {
+                cancelTokenSource.Dispose();
+            }
+
             //  проверяем статус задачи
             Console.WriteLine($"Task Status: {task.Status}");
-            cancelTokenSource.Dispose(); // освобождаем ресурсы
         }
     }
 }
