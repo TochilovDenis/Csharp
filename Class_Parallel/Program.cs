52785086ff1a667b431c14bddc2711eb4363576d
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-28 19:38:43 +0300

    Класс Parallel также является частью TPL и предназначен для упрощения параллельного выполнения кода. Parallel имеет ряд методов, которые позволяют распараллелить задачу.
    Одним из методов, позволяющих параллельное выполнение задач, является метод Invoke.
    
    Метод Parallel.Invoke в качестве параметра принимает массив объектов Action, то есть мы можем передать в данный метод набор методов, которые будут вызываться при его выполнении. Количество методов может быть различным, но в данном случае мы определяем выполнение трех методов. Опять же как и в случае с классом Task мы можем передать либо название метода, либо лямбда-выражение.
    
    И таким образом, при наличии нескольких ядер на целевой машине данные методы будут выполняться параллельно на различных ядрах. Пример консольного вывода программы:
    Выполняется задача 3
    Выполняется задача 1
    Выполняется задача 2
    Результат 25

--- Class_Parallel/Program.cs
+++ Class_Parallel/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Class_Parallel
@@ -10,6 +11,27 @@ namespace Class_Parallel
     {
         static void Main(string[] args)
         {
+            // метод Parallel.Invoke выполняет три метода
+            Parallel.Invoke( Print, () =>
+                {
+                    Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+                    Thread.Sleep(3000);
+                },
+                () => Square(5)
+            );
+
+            void Print()
+            {
+                Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+                Thread.Sleep(3000);
+            }
+            // вычисляем квадрат числа
+            void Square(int n)
+            {
+                Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+                Thread.Sleep(3000);
+                Console.WriteLine($"Результат {n * n}");
+            }
         }
     }
 }
