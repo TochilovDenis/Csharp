2046a499768478e2c6c6389d4ed8a12035c5a0ba
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-28 19:33:58 +0300

    task is solved

--- Continuation_tasks_result/Program.cs
+++ Continuation_tasks_result/Program.cs
@@ -10,6 +10,22 @@ namespace Continuation_tasks_result
     {
         static void Main(string[] args)
         {
-        }
+            Task<int> Tasks = new Task<int>(() => Square(2));
+
+            Task<int> Tasks_1 = Tasks.ContinueWith(task => Square(task.Result));
+            Task<int> Tasks_2 = Tasks_1.ContinueWith(task => Square(task.Result));
+            Task<int> Tasks_3 = Tasks_2.ContinueWith(task => Square(task.Result));
+
+            Tasks.Start();
+            Tasks_3.Wait();
+            Console.WriteLine(Tasks_3.Result);
+
+            int Square(int a)
+            {
+                Console.WriteLine(a);
+                return a* a;
+            };
+          
+        }       
     }
 }
