887e106cdd4f68af417f280364d0fbc40b742c36
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-28 19:01:44 +0300

    Задание: 1 Создать функцию которая перемножает число само на себя 2 С помощью задач продолжения довести 2 до 65 536 (2^2^2^2^2). Передавая результат выполнения функции в задачу продолжения

---
+++ Continuation_tasks_result/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continuation_tasks_result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
