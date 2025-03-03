7a765cb2732a6821bfbb003831f88e5ee3adcc95
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 19:08:16 +0300

    Преимущества асинхронности Данный код является синхронным и выполняет последовательно три вызова метода PrintName. Поскольку для имитации продолжительной работы в методе установлена задержка на три секунды, то общее выполнение программы займет не менее 9 секунд. Так как каждый последующий вызов PrintName будет ждать пока завершится предыдущий.

--- Asynchronous_programming/Program.cs
+++ Asynchronous_programming/Program.cs
@@ -11,8 +11,16 @@ namespace Asynchronous_programming
     {
         static async Task Main(string[] args)
         {
+            PrintName("Tom");
+            PrintName("Bob");
+            PrintName("Sam");
 
-           
+            void PrintName(string name)
+            {
+                Thread.Sleep(3000);     // имитация продолжительной работы
+                Console.WriteLine(name);
+            }
+
         }
     }
 }
