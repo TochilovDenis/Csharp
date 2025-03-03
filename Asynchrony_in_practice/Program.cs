8b6f0292621a73edae5dd130b9b9899ed338d968
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 20:46:55 +0300

    Задача "Асинхронные силачи": Необходимо сделать имитацию соревнований по поднятию шаров Атласа. Напишите асинхронную функцию start_strongman(name, power), где name - имя силача, power - его подъёмная мощность. Реализуйте следующую логику в функции:
    В начале работы должна выводиться строка - "Силач <имя силача> начал соревнования."
    После должна выводиться строка - "Силач <имя силача> поднял <номер шара>" с задержкой обратно пропорциональной его силе power. Для каждого участника количество шаров одинаковое - 5.
    В конце поднятия всех шаров должна выводится строка "Силач <имя силача> закончил соревнования."
    Также напишите асинхронную функцию start_tournament, в которой создаются 3 задачи для функций start_strongman. Имена (name) и силу (power) для вызовов функции start_strongman можете выбрать самостоятельно.
    
    После поставьте каждую задачу в ожидание (await).
    
    Запустите асинхронную main.
    
    Пример результата выполнения программы:
    
    Переданные аргументы в функции start_strongman:
    
    "Pasha", 3
    "Denis", 4
    "Apollon", 5
    
    Вывод на консоль:
    
    Силач Apollon поднял 1 шар
    Силач Denis поднял 1 шар
    Силач Pasha поднял 1 шар
    Силач Apollon поднял 2 шар
    Силач Denis поднял 2 шар
    Силач Apollon поднял 3 шар
    Силач Pasha поднял 2 шар
    Силач Denis поднял 3 шар
    Силач Apollon поднял 4 шар
    Силач Pasha поднял 3 шар
    Силач Apollon поднял 5 шар
    Силач Denis поднял 4 шар
    Силач Apollon поднял 6 шар
    Силач Apollon закончил соревнования.
    -----------------------------------------
    Силач Denis поднял 5 шар
    Силач Pasha поднял 4 шар
    Силач Denis поднял 6 шар
    Силач Denis закончил соревнования.
    -----------------------------------------
    Силач Pasha поднял 5 шар
    Силач Pasha поднял 6 шар
    Силач Pasha закончил соревнования.
    -----------------------------------------

--- Asynchrony_in_practice/Program.cs
+++ Asynchrony_in_practice/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Asynchrony_in_practice
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            // Имитация соревнований по поднятию шаров Атласа
+            var task1 = Start_strongman("Pasha", 3);
+            var task2 = Start_strongman("Denis", 4);
+            var task3 = Start_strongman("Apollon", 5);
+
+            await Task.WhenAll(task1, task2, task3);
+
+            async Task Start_strongman(String name, int power)
+            {
+                // name - имя силача
+                // power - подъёмная мощность
+                Console.WriteLine($"Силач {name} начал соревнования");
+                for (int i = 0; i <= 5; i++)
+                {
+                    await Task.Delay((1000/ power));
+                    Console.WriteLine($"Силач {name} поднял {i + 1} шар");                    
+                }
+                Console.WriteLine($"Силач {name} закончил соревнования.");
+                Console.WriteLine("-----------------------------------------");
+            }           
         }
     }
 }
