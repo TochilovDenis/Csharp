d46488b558ee715cc00e9a79d97529c4810d584f
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 20:11:28 +0300

    clear

--- Sequential_and_parallel_execution/Program.cs
+++ Sequential_and_parallel_execution/Program.cs
@@ -10,19 +10,6 @@ namespace Sequential_and_parallel_execution
     {
         static async Task Main(string[] args)
         {
-            // определяем и запускаем задачи
-            var task1 = PrintAsync("Hello C#");
-            var task2 = PrintAsync("Hello World");
-            var task3 = PrintAsync("Hello METANIT.COM");
-
-            // ожидаем завершения хотя бы одной задачи
-            await Task.WhenAny(task1, task2, task3);
-
-            async Task PrintAsync(string message)
-            {
-                await Task.Delay(new Random().Next(1000, 2000));     // имитация продолжительной операции
-                Console.WriteLine(message);
-            }
         }
     }
 }
