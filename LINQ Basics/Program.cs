ad5de05815c1ead58b48f06e4df9938392214cd6
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 20:56:58 +0300

    Операторы запросов LINQ Операторы запросов LINQ в каком-то роде частично напоминают синтаксис запросов SQL, поэтому если вы работали когда-нибудь с sql-запросами, то будет проще понять общую концепцию. Итак, изменим предыдущий пример, применив операторы запросов LINQ: Прежде всего, как мы видим, код стал меньше и проще, а результат будет тем же. В принципе все выражение можно было бы записать в одну строку:
       var selectedPeople = from p in people where p.ToUpper().StartsWith("T") orderby p  select p;
    
    Но для более понятной логической разбивки я поместил каждое отдельное подвыражение на отдельной строке.
    
    Простейшее определение запроса LINQ выглядит следующим образом:
    
      | from переменная in набор_объектов |
      | select переменная;                |
    Итак, что делает этот запрос LINQ? Выражение from p in people проходит по всем элементам массива people и определяет каждый элемент как p. Используя переменную p мы можем проводить над ней разные операции.
    
    Несмотря на то, что мы не указываем тип переменной p, выражения LINQ являются строго типизированными. То есть среда автоматически распознает, что набор people состоит из объектов string, поэтому переменная p будет рассматриваться в качестве строки.
    
    Далее с помощью оператора where проводится фильтрация объектов, и если объект соответствует критерию (в данном случае начальная буква должна быть "T"), то этот объект передается дальше.
    
    Оператор orderby упорядочивает по возрастанию, то есть сортирует выбранные объекты.
    
    Оператор select передает выбранные значения в результирующую выборку, которая возвращается LINQ-выражением.
    
    В данном случае результатом выражения LINQ является объект IEnumerable<T>. Нередко результирующая выборка определяется с помощью ключевого слова var, тогда компилятор на этапе компиляции сам выводит тип.

--- LINQ Basics/Program.cs
+++ LINQ Basics/Program.cs
@@ -13,16 +13,10 @@ namespace LINQ_Basics
             string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill" };
 
             // создаем новый список для результатов
-            var selectedPeople = new List<string>();
-            // проходим по массиву
-            foreach (string person in people)
-            {
-                // если строка начинается на букву T, добавляем в список
-                if (person.ToUpper().StartsWith("T"))
-                    selectedPeople.Add(person);
-            }
-            // сортируем список
-            selectedPeople.Sort();
+            var selectedPeople = from p in people // передаем каждый элемент из people в переменную p
+                                 where p.ToUpper().StartsWith("T") //фильтрация по критерию
+                                 orderby p  // упорядочиваем по возрастанию
+                                 select p; // выбираем объект в создаваемую коллекцию
 
             foreach (string person in selectedPeople)
                 Console.WriteLine(person);
