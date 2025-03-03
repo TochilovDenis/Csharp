bcf8fd10723c28cf0876b1a64db97a2143f54a9d
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 19:18:12 +0300

    void При использовании ключевого слова void асинхронный метод ничего не возвращает. Однако асинхронных void-методов следует избегать и следует использовать только там, где эти подобные методы представляют единственный возможный способ определения асинхронного метода. Прежде всего, мы не можем применить к подобным методам оператор await. Также потому что исключения в таких методах сложно обрабатывать, так как они не могут быть перехвачены вне метода. Кроме того, подобные void-методы сложно тестировать.

---
+++ Returning_a_result_from_an_asynchronous_method/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Returning_a_result_from_an_asynchronous_method
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            PrintAsync("Hello World");
+            PrintAsync("Hello METANIT.COM");
+
+            Console.WriteLine("Main End");
+            await Task.Delay(3000); // ждем завершения задач
+
+            // определение асинхронного метода
+            async void PrintAsync(string message)
+            {
+                await Task.Delay(1000);     // имитация продолжительной работы
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ Returning_a_result_from_an_asynchronous_method/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Returning_a_result_from_an_asynchronous_method")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Returning_a_result_from_an_asynchronous_method")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9e9c2d0d-0d15-4069-b795-669d4edb7a14")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
