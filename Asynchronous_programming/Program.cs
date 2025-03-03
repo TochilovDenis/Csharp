c5c8c2d10fabe407d85eed22548c1975a975e5ac
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 19:06:15 +0300

    clear

--- Asynchronous_programming/Program.cs
+++ Asynchronous_programming/Program.cs
@@ -12,30 +12,7 @@ namespace Asynchronous_programming
         static async Task Main(string[] args)
         {
 
-            //слово async, которое указывается в определении метода, НЕ делает автоматически метод асинхронным.
-            //Оно лишь указывает, что данный метод может содержать одно или несколько выражений await.
-
-            Console.WriteLine("Начало выполнения программы");
-
-            await PrintAsync();
-
-            Console.WriteLine("Конец выполнения программы");
-
-
-            void Print()
-            {
-                Thread.Sleep(10000);
-                Console.WriteLine("Метод Print");
-            }
-
-            async Task PrintAsync()
-            {
-                Console.WriteLine("Начало метода PrintAsync");
-                await Task.Delay(3000);
-               // await Task.Run(Print);
-                
-                Console.WriteLine("Конец метода PrintAsync");
-            }
+           
         }
     }
 }
