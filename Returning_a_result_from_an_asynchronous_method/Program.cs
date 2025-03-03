e5d0e2626365d5cabe2b778974cd51da43c23756
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 19:39:07 +0300

    clear

--- Returning_a_result_from_an_asynchronous_method/Program.cs
+++ Returning_a_result_from_an_asynchronous_method/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,25 +11,7 @@ namespace Returning_a_result_from_an_asynchronous_method
     {
         static async Task Main(string[] args)
         {
-            var square5 = SquareAsync(5);
-            var square6 = SquareAsync(6);
-
-            Console.WriteLine("Остальные действия в методе Main");
-
-            Console.WriteLine("Тип переменной square5: " + square5.GetType());
-            Console.WriteLine("Тип переменной await square5: " + (await square5).GetType());
-
-            int n1 = await square5;
-            int n2 = await square6;
-            Console.WriteLine($"n1={n1}  n2={n2}"); // n1=25  n2=36
-
-            async Task<int> SquareAsync(int n)
-            {
-                await Task.Delay(0);
-                var result = n * n;
-                Console.WriteLine($"Квадрат числа {n} равен {result}");
-                return result;
-            }
+            
         }
     }
 }
