00897f03eece8628747b45a0827f1098d212d261
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 20:53:12 +0300

    В чем же удобство LINQ? Посмотрим на простейшем примере. Выберем из массива строки, которые начинаются на определенную букву, например, букву "T", и отсортируем полученный список:
    Для отфильтрованных строк создается специальный список. Затем в цикле проходим по всем элементам массива и, если они соответствуют условию (начинаются на букву T), то добавляем их в этот список. Затем сортируем список по возрастанию. И в конце элементы полученного списка выводим на консоль:
    
    Tim
    Tom
    Tomas

--- LINQ Basics/Program.cs
+++ LINQ Basics/Program.cs
@@ -10,6 +10,22 @@ namespace LINQ_Basics
     {
         static void Main(string[] args)
         {
+            string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill" };
+
+            // создаем новый список для результатов
+            var selectedPeople = new List<string>();
+            // проходим по массиву
+            foreach (string person in people)
+            {
+                // если строка начинается на букву T, добавляем в список
+                if (person.ToUpper().StartsWith("T"))
+                    selectedPeople.Add(person);
+            }
+            // сортируем список
+            selectedPeople.Sort();
+
+            foreach (string person in selectedPeople)
+                Console.WriteLine(person);
         }
     }
 }
