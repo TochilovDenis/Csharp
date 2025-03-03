1b72d436daaaadb5c75cf63a4a27070e54e63022
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 21:07:43 +0300

    Приложение считывает набор целых значений в список.  Необходимо посчитать количество уникальных значений.  Для решения задачи используйте возможности LINQ.

---
+++ task_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] age = {1, 2, 5, 111, 3, 5, 9, 28, 5, 28, 1 };
+
+            var selectPeople = age.Distinct();
+
+            foreach (int a in selectPeople)
+                Console.WriteLine(a);
+        }
+    }
+}
---
+++ task_01/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("task_01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task_01")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f11daf71-b4fb-4e54-be6f-25d689e48690")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
