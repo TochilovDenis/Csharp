11a42d8c7dc57d6de38dda153eb76e65e6716416
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 20:04:32 +0300

    Асинхронный метод может содержать множество выражений await. Когда система встречает в блоке кода оператор await, то выполнение в асинхронном методе останавливается, пока не завершится асинхронная задача. После завершения задачи управление переходит к следующему оператору await и так далее. Это позволяет вызывать асинхронные задачи последовательно в определенном порядке.

--- Sequential_and_parallel_execution/Program.cs
+++ Sequential_and_parallel_execution/Program.cs
@@ -8,8 +8,17 @@ namespace Sequential_and_parallel_execution
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            await PrintAsync("Hello C#");
+            await PrintAsync("Hello World");
+            await PrintAsync("Hello METANIT.COM");
+
+            async Task PrintAsync(string message)
+            {
+                await Task.Delay(2000);     // имитация продолжительной операции
+                Console.WriteLine(message);
+            }
         }
     }
 }
