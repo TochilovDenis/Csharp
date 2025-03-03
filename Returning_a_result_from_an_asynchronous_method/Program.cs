212910530a03b316b5531069059e3f2270b5c742
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 19:25:57 +0300

    clear

--- Returning_a_result_from_an_asynchronous_method/Program.cs
+++ Returning_a_result_from_an_asynchronous_method/Program.cs
@@ -10,29 +10,6 @@ namespace Returning_a_result_from_an_asynchronous_method
     {
         static async Task Main(string[] args)
         {
-            Account account = new Account();
-            account.Added += PrintAsync;
-
-            account.Put(500);
-
-            await Task.Delay(2000); // ждем завершения
-
-            // определение асинхронного метода
-            async void PrintAsync(object obj, string message)
-            {
-                await Task.Delay(1000);     // имитация продолжительной работы
-                Console.WriteLine(message);
-            }
-        }
-        class Account
-        {
-            int sum = 0;
-            public event EventHandler<string> Added;
-            public void Put(int sum)
-            {
-                this.sum += sum;
-                Added?.Invoke(this, $"На счет поступило {sum} $");
-            }
         }
     }
 }
