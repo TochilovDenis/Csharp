d422e1fe4386047de84f4558342ecdc22915aa0b
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 20:13:04 +0300

    Получение результата Задачи, передаваемые в Task.WhenAll и Task.WhenAny, могут возвращать некоторое значение. В этом случае из методов Task.WhenAll и Task.WhenAny можно получить массив, который будет содержать результаты задач.
    В данном случае метод Square возвращает число int - квадрат передаваемого в метод числа. И переменная results будет содержать результат вызова Task.WhenAll - по сути результаты всех трех запущенных задач. Поскольку все передаваемые в Task.WhenAll задачи возвращают int, то соответственно результат Task.WhenAll будет представлять массив значений int.

--- Sequential_and_parallel_execution/Program.cs
+++ Sequential_and_parallel_execution/Program.cs
@@ -10,6 +10,22 @@ namespace Sequential_and_parallel_execution
     {
         static async Task Main(string[] args)
         {
+            // определяем и запускаем задачи
+            var task1 = SquareAsync(4);
+            var task2 = SquareAsync(5);
+            var task3 = SquareAsync(6);
+
+            // ожидаем завершения всех задач
+            int[] results = await Task.WhenAll(task1, task2, task3);
+            // получаем результаты:
+            foreach (int result in results)
+                Console.WriteLine(result);
+
+            async Task<int> SquareAsync(int n)
+            {
+                await Task.Delay(1000);
+                return n * n;
+            }
         }
     }
 }
