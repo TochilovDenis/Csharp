2acb88b3efd6bc8035f512d613b680972f9dc057
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-03 18:58:47 +0300

    https://metanit.com/sharp/tutorial/13.3.php

---
+++ Asynchronous_programming/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynchronous_programming
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Начало выполнения программы");
+
+            PrintAsync();
+
+            Console.WriteLine("Конец выполнения программы");
+
+
+            void Print()
+            {
+                Thread.Sleep(10000);
+                Console.WriteLine("Метод Print");
+            }
+
+            async Task PrintAsync()
+            {
+                Console.WriteLine("Начало метода PrintAsync");
+
+                Task.Run(Print);
+                
+                Console.WriteLine("Конец метода PrintAsync");
+            }
+        }
+    }
+}
---
+++ Asynchronous_programming/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Asynchronous_programming")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Asynchronous_programming")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("924b6a17-a60c-475c-a9f3-e86d2a2ef554")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
