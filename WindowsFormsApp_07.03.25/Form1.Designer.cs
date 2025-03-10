45586b75f583b8b0ef615855887fa5df45272f86
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-10 19:21:26 +0300

    ...

--- WindowsFormsApp_07.03.25/Form1.Designer.cs
+++ WindowsFormsApp_07.03.25/Form1.Designer.cs
@@ -92,7 +92,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(561, 56);
+            this.button1.Location = new System.Drawing.Point(576, 56);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(105, 32);
             this.button1.TabIndex = 5;
@@ -141,7 +141,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(669, 12);
+            this.button2.Location = new System.Drawing.Point(684, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(105, 32);
             this.button2.TabIndex = 10;
@@ -152,7 +152,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Location = new System.Drawing.Point(672, 56);
+            this.button3.Location = new System.Drawing.Point(687, 56);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(105, 32);
             this.button3.TabIndex = 11;
