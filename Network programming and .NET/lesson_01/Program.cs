969446f38eec29b96d6ac9a2262b9d602df77bf8
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-25 17:38:58 +0300

    ...

--- Network programming and .NET/lesson_01/Program.cs
+++ Network programming and .NET/lesson_01/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
 
 namespace lesson_01
 {
@@ -10,6 +9,28 @@ namespace lesson_01
     {
         static void Main(string[] args)
         {
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(ip, 1024);
+            s.Bind(ep);
+            s.Listen(10);
+            try
+            {
+                while (true)
+                {
+                    Socket ns = s.Accept();
+                    Console.WriteLine(ns.RemoteEndPoint.ToString());
+                    ns.Send(System.Text.Encoding.ASCII.
+                    GetBytes(DateTime.Now.ToString()));
+                    ns.Shutdown(SocketShutdown.Both);
+                    ns.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
